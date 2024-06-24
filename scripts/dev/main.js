ce8811341c2c131b2016c927d26aa2cba0b544ad
ZhannaLak <160404743+ZhannaLak@users.noreply.github.com>
2024-06-24 21:16:04 +0700

    js

---
+++ scripts/dev/main.js
@@ -0,0 +1,84 @@
+(function () {
+    "use strict";
+
+
+    const root = document.documentElement;
+    const navToggle = document.querySelector("#js-navToggle");
+    navToggle.addEventListener("click", function () {
+        root.classList.toggle("show-nav");
+    });
+
+
+    const eventPP = document.querySelector("#js-eventPP");
+    if (eventPP) {
+        const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+        eventOpenBtn.addEventListener("click", function () {
+            root.classList.add("show-event-popup");
+        });
+
+        eventPP.addEventListener("click", function (event) {
+            if (
+                event.target === this ||
+                event.target.classList.contains("js-ppCloseBtn")
+            ) {
+                root.classList.remove("show-event-popup");
+            }
+        });
+        document.addEventListener("keyup", function (event) {
+            if (event.key === "Escape" || event.keyCode === 27) {
+                root.classList.remove("show-event-popup");
+            }
+        });
+
+        const eventPP = document.querySelector("#js-eventPP");
+        if (eventPP) {
+            const eventOpenBtn = document.querySelector("#js-eventOpenBtn");
+            const closeEventPP = function (event) {
+                function close() {
+                    document.removeEventListener("keyup", closeEventPP);
+                    eventPP.removeEventListener("click", closeEventPP);
+                    root.classList.remove("show-event-popup");
+                }
+                switch (event.type) {
+                    case "keyup":
+                        if (event.key === "Escape" || event.keyCode === 27) close();
+                        break;
+                    case "click":
+                        if (
+                            event.target === this ||
+                            event.target.classList.contains("js-ppCloseBtn")
+                        )
+                            close();
+                        break;
+                }
+            };
+            eventOpenBtn.addEventListener("click", function () {
+                root.classList.add("show-event-popup");
+                document.addEventListener("keyup", closeEventPP);
+                eventPP.addEventListener("click", closeEventPP);
+            });
+        }
+    }
+
+    const swipers = document.querySelectorAll(".js-swiper");
+    swipers.forEach(function (swpr) {
+      new Swiper(swpr, {
+        updateOnWindowResize: true,
+        slidesPerView: "auto",
+        freeMode: true,
+        spaceBetween: 0,
+        speed: 500,
+        grabCursor: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-arrow-next",
+          prevEl: ".swiper-arrow-prev",
+          disabledClass: "arrow--disabled"
+        }
+      });
+    });
+
+})();
